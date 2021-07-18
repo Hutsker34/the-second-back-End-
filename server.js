7eb008e78026d669afca112e7f4850a2a2030881
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2021-07-17 19:43:23 -0700

    сделали отображение каждого сообщения в каждой группе

--- server.js
+++ server.js
@@ -11,18 +11,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 let history = {}
 try {
-  history=JSON.parse(fs.readFileSync('out.json','utf8'))
-  console.log(history)
+  history = JSON.parse(fs.readFileSync('out.json', 'utf8'))
+  
 } catch (error) {
   console.log('что-то пошло не так!')
 }
-app.post('/user', (req, res) => {
-  if(history[req.body.id]){history[req.body.id].push(req.body.history) }
-  else {
-    history[req.body.id]=[req.body.history]
+app.post('/create-mess', (req, res) => {
+  if (history[req.body.id]) { 
+   history[req.body.id].push(req.body.history) 
   }
-  console.log(req.body)
-  fs.writeFile('out.json', JSON.stringify(history,null,2), 'utf8', () => {
+  else {
+    history[req.body.id] = [req.body.history]
+  }
+  
+  fs.writeFile('out.json', JSON.stringify(history, null, 2), 'utf8', () => {
     console.log('Мы записали данные, ура!')
   });
   res.send(`hellow world ${port}`)
@@ -31,8 +33,18 @@ app.post('/user', (req, res) => {
 // listen on the port
 app.listen(port);
 
-app.post('/history',(req, res) =>{
+app.post('/history', (req, res) => {
   res.send(history)
-  });
-  
+});
+
+app.post('/get-dialogue',(req,res) => {
+  const idDialogue = req.body.id
+  if (idDialogue in history) {
+    res.send(history[idDialogue])
+  } else {
+    res.send([])
+  }
+});
+
+
 
